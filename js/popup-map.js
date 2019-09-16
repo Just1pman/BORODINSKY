a0685595413dacdb0463f7bd13ccd0b9dd24242b
Alexandr <just1pman@gmail.com>
2019-09-16 20:22:15 +0300

    Создали репозиторий с готовым проектом

---
+++ js/popup-map.js
@@ -0,0 +1,29 @@
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".modal-close");
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+mapLink.addEventListener("click", function (evt){
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+
+        if (mapPopup.classList.contains("modal-show")) {
+            mapPopup.classList.remove("modal-show");
+        }
+    }
+});
---
+++ js/popup.js
@@ -0,0 +1,53 @@
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=login]");
+var form = popup.querySelector("form");
+var close = document.querySelector(".modal-close");
+var isStorageSupport = true;
+var storage = localStorage.getItem("login");
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt){
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!login.value, !password.value) {
+        evt.preventDefault();
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+})
